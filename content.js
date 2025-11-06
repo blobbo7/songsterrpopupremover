7a6003e9fe20b0678f92601abad227943ead0432
blobbo <84816363+blobbo7@users.noreply.github.com>
2025-11-05 17:12:27 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,22 +1,4 @@
-function clickDismissLink() {
-    const dismissLink = Array.from(document.querySelectorAll('a[href=""]'))
-        .find(el => el.textContent.trim().toLowerCase() === 'continue with');
-
-    if (dismissLink && dismissLink.offsetParent !== null) {
-        dismissLink.click();
-        console.log("✅ Songsterr popup dismissed.");
-    }
-}
-
-function cleanupOverlay() {
-    const overlay = document.querySelector('.hide-layer-overlay');
-    if (overlay) overlay.remove();
-
-    document.body.style.overflow = 'auto';
-}
-
-// Try every half second
 setInterval(() => {
-    clickDismissLink();
-    cleanupOverlay();
+  const link = document.querySelector('p.C83v7g a');
+  if (link) link.click();
 }, 100);
